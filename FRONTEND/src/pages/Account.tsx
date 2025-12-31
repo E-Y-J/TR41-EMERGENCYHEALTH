deb8206e5ceda62394a94e8184071832ea195899
Jessica <jbunds@proton.me>
2025-12-31 16:11:32 -0700

    Reorganized account forms into Forms directory and updated Account page structure

--- FRONTEND/src/pages/Account.tsx
+++ FRONTEND/src/pages/Account.tsx
@@ -1,8 +1,24 @@
+import Allergies from '../components/Forms/Allergies'
+import Conditions from '../components/Forms/Conditions';
+import Medications from '../components/Forms/Medications';
+import PersonalInfo from '../components/Forms/PersonalInfo';
 
 const Account = () => {
     return (
         <div className="text-center">
             <p>Account coming soon</p>
+            <div>
+                <PersonalInfo />
+            </div>
+            <div>
+                <Allergies />
+            </div>
+            <div>
+                <Medications />
+            </div>
+            <div>
+                <Conditions />
+            </div>
         </div>
     );
 };
